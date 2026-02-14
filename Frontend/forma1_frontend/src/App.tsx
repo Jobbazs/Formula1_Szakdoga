d54e76a7d640bf9c3e7f4d8a29bb670079cd4d76
Jobbazs <grega.balazs@gmail.com>
2026-02-14 14:57:51 +0100

    main push

--- Frontend/forma1_frontend/src/App.tsx
+++ Frontend/forma1_frontend/src/App.tsx
@@ -7,11 +7,52 @@ import {GrandPrixPage} from './pages/grand_prix';
 import './styles/navbar.css'; 
 import './styles/home.css';
 
+async function login() {
+  console.log("login start");
+
+  // CSRF cookie lekérése
+  await fetch("http://localhost:8000/sanctum/csrf-cookie", {
+    method: "GET",
+    credentials: "include"
+  });
+
+  // CSRF token kinyerése cookie-ból
+  const getCookie = (name: string) => {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return decodeURIComponent(match[2]);
+    return null;
+  };
+  const csrfToken = getCookie('XSRF-TOKEN');
+//Ezt a login logikát csak át kell majd másolni majd a login oldalunkra. működik a login, jó a cookie, be lehet jelentkezni a lent látható admin felhasználóval.
+  // Login request
+  const response = await fetch("http://localhost:8000/api/login", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "X-XSRF-TOKEN": csrfToken || ""
+    },
+    body: JSON.stringify({
+      email: "admin@admin.hu",
+      password: "Aa123456"
+    })
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
+
 const App: React.FC = () => {
   return (
     <Router>
       <Navbar />
-      
+
+      <button onClick={login}>Login</button>
+
+
+
       <div className="content">
         <Routes>
           <Route path="/" element={<SmartButtons />} />
